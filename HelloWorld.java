05a33865dda76678258230ec02c1094304f66c39
Shanmugam Murugesan <kannappar@Shanmugams-MacBook-Pro.local>
2020-09-25 22:39:56 -0700

    add

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,5 @@
 public class HelloWorld {
-	   private String message;
-
-	   public void setMessage(String message){
-	      this.message  = message;
-	   }
-	   public void getMessage(){
-	      System.out.println("Your Message : " + message);
-	   }
+public static void main(String[] args) {
+System.out.println("Welcome to Jenkins");
+}
 	}
